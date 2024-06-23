4270e0b118d0e8b349331828bf6345fecaac36b7
heochangwon <wonn22@gmail.com>
2024-06-23 14:17:51 +0900

    component-composition으로 해결하기

--- 10 Advanced State Management with Context useReducer/01-starting-project/src/App.jsx
+++ 10 Advanced State Management with Context useReducer/01-starting-project/src/App.jsx
@@ -1,8 +1,9 @@
-import { useState } from 'react';
+import { useState } from "react";
 
-import Header from './components/Header.jsx';
-import Shop from './components/Shop.jsx';
-import { DUMMY_PRODUCTS } from './dummy-products.js';
+import Header from "./components/Header.jsx";
+import Shop from "./components/Shop.jsx";
+import Product from "./components/Product.jsx";
+import { DUMMY_PRODUCTS } from "./dummy-products.js";
 
 function App() {
   const [shoppingCart, setShoppingCart] = useState({
@@ -71,7 +72,15 @@ function App() {
         cart={shoppingCart}
         onUpdateCartItemQuantity={handleUpdateCartItemQuantity}
       />
-      <Shop onAddItemToCart={handleAddItemToCart} />
+      <Shop>
+        <ul id="products">
+          {DUMMY_PRODUCTS.map((product) => (
+            <li key={product.id}>
+              <Product {...product} onAddToCart={onAddItemToCart} />
+            </li>
+          ))}
+        </ul>
+      </Shop>
     </>
   );
 }
