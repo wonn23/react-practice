6b49cb806e7a7db35c46aca4888e2a211d67e0be
heochangwon <wonn22@gmail.com>
2024-06-23 22:53:16 +0900

    Action으로 내보내기 & useReducer로 State 수정하기

--- 10 Advanced State Management with Context useReducer/01-starting-project/src/App.jsx
+++ 10 Advanced State Management with Context useReducer/01-starting-project/src/App.jsx
@@ -1,78 +1,10 @@
-import { useState } from "react";
-
 import Header from "./components/Header.jsx";
 import Shop from "./components/Shop.jsx";
 import Product from "./components/Product.jsx";
 import { DUMMY_PRODUCTS } from "./dummy-products.js";
-import { CartContext } from "./store/shopping-cart-context.jsx";
+import CartContextProvider from "./store/shopping-cart-context.jsx";
 
 function App() {
-  const [shoppingCart, setShoppingCart] = useState({
-    items: [],
-  });
-
-  function handleAddItemToCart(id) {
-    setShoppingCart((prevShoppingCart) => {
-      const updatedItems = [...prevShoppingCart.items];
-
-      const existingCartItemIndex = updatedItems.findIndex(
-        (cartItem) => cartItem.id === id
-      );
-      const existingCartItem = updatedItems[existingCartItemIndex];
-
-      if (existingCartItem) {
-        const updatedItem = {
-          ...existingCartItem,
-          quantity: existingCartItem.quantity + 1,
-        };
-        updatedItems[existingCartItemIndex] = updatedItem;
-      } else {
-        const product = DUMMY_PRODUCTS.find((product) => product.id === id);
-        updatedItems.push({
-          id: id,
-          name: product.title,
-          price: product.price,
-          quantity: 1,
-        });
-      }
-
-      return {
-        items: updatedItems,
-      };
-    });
-  }
-
-  function handleUpdateCartItemQuantity(productId, amount) {
-    setShoppingCart((prevShoppingCart) => {
-      const updatedItems = [...prevShoppingCart.items];
-      const updatedItemIndex = updatedItems.findIndex(
-        (item) => item.id === productId
-      );
-
-      const updatedItem = {
-        ...updatedItems[updatedItemIndex],
-      };
-
-      updatedItem.quantity += amount;
-
-      if (updatedItem.quantity <= 0) {
-        updatedItems.splice(updatedItemIndex, 1);
-      } else {
-        updatedItems[updatedItemIndex] = updatedItem;
-      }
-
-      return {
-        items: updatedItems,
-      };
-    });
-  }
-
-  const ctxValue = {
-    items: shoppingCart.items,
-    addItemToCart: handleAddItemToCart,
-    updateItemQuantity: handleUpdateCartItemQuantity,
-  };
-
   return (
     /**
      *  createContext를 Header와 Shop을 감싸는 이유는
@@ -81,18 +13,18 @@ function App() {
      *  컨텍스트를 생성할 때 설정한 기본 값은 Provider 컴포넌트로 감싸지지 않은 컴포넌트가 컨텍스트 값을 액세스하려고 할 때만 사용됩니다.
      *  context value를 useState와 연결시키는 방법은
      */
-    <CartContext.Provider value={ctxValue}>
+    <CartContextProvider>
       <Header />
       <Shop>
         <ul id="products">
           {DUMMY_PRODUCTS.map((product) => (
             <li key={product.id}>
-              <Product {...product} onAddToCart={handleAddItemToCart} />
+              <Product {...product} />
             </li>
           ))}
         </ul>
       </Shop>
-    </CartContext.Provider>
+    </CartContextProvider>
   );
 }
 
--- 10 Advanced State Management with Context useReducer/01-starting-project/src/store/shopping-cart-context.jsx
+++ 10 Advanced State Management with Context useReducer/01-starting-project/src/store/shopping-cart-context.jsx
@@ -1,5 +1,6 @@
-import { createContext } from "react";
+import { createContext, useReducer, useState } from "react";
 
+import { DUMMY_PRODUCTS } from "../dummy-products.js";
 /**
  *  컨텍스트의 초기값을 createContext안에 넣어준다.
  *  context를 적용할 컴포넌트들도 묶어줘야한다.
@@ -9,3 +10,94 @@ export const CartContext = createContext({
   addItemToCart: () => {}, // 값 업데이트하는 함수
   updateItemQuantity: () => {},
 });
+
+function shoppingCartReducer(state, action) {
+  if (action.type === "ADD_ITEM") {
+    const updatedItems = [...state.items];
+
+    const existingCartItemIndex = updatedItems.findIndex(
+      (cartItem) => cartItem.id === action.payload
+    );
+    const existingCartItem = updatedItems[existingCartItemIndex];
+
+    if (existingCartItem) {
+      const updatedItem = {
+        ...existingCartItem,
+        quantity: existingCartItem.quantity + 1,
+      };
+      updatedItems[existingCartItemIndex] = updatedItem;
+    } else {
+      const product = DUMMY_PRODUCTS.find(
+        (product) => product.id === action.payload
+      );
+      updatedItems.push({
+        id: action.payload,
+        name: product.title,
+        price: product.price,
+        quantity: 1,
+      });
+    }
+
+    return {
+      ...state, // value가 하나라면 필요없지만, 업데이트할 내용이 많고 updatedItems만 업데이트 하고 싶을 때 이렇게 작성해야한다.
+      items: updatedItems,
+    };
+  }
+
+  if (action.type === "UPDATE_ITEM") {
+    const updatedItems = [...state.items];
+    const updatedItemIndex = updatedItems.findIndex(
+      (item) => item.id === action.payload.productId
+    );
+
+    const updatedItem = {
+      ...updatedItems[updatedItemIndex],
+    };
+
+    updatedItem.quantity += action.payload.amount;
+
+    if (updatedItem.quantity <= 0) {
+      updatedItems.splice(updatedItemIndex, 1);
+    } else {
+      updatedItems[updatedItemIndex] = updatedItem;
+
+      return {
+        ...state, // 새로운 객체를 만들 때, 이전 데이터의 손실이 없도록 한번 더 지난 상태를 복사해준다.
+        items: updatedItems,
+      };
+    }
+  }
+  return state;
+}
+
+export default function CartContextProvider({ children }) {
+  const [shoppingCartState, shoppingCartDispatch] = useReducer(
+    shoppingCartReducer,
+    { items: [] }
+  );
+
+  function handleAddItemToCart(id) {
+    shoppingCartDispatch({
+      type: "ADD_ITEM",
+      payload: id,
+    });
+  }
+
+  function handleUpdateCartItemQuantity(productId, amount) {
+    shoppingCartDispatch;
+    ({
+      type: "UPDATE_ITEM",
+      payload: { productId, amount },
+    });
+  }
+
+  const ctxValue = {
+    items: shoppingCartState.items,
+    addItemToCart: handleAddItemToCart,
+    updateItemQuantity: handleUpdateCartItemQuantity,
+  };
+
+  return (
+    <CartContext.Provider value={ctxValue}>{children}</CartContext.Provider>
+  );
+}
