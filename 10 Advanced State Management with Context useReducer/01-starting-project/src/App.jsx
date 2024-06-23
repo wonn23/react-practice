1abbfdbe40b25c53d68db262aeb4b9c0c3cf964a
heochangwon <wonn22@gmail.com>
2024-06-23 18:11:03 +0900

    Context와 State 연결하기: Context에 업데이트하는 함수도 연결해야함

--- 10 Advanced State Management with Context useReducer/01-starting-project/src/App.jsx
+++ 10 Advanced State Management with Context useReducer/01-starting-project/src/App.jsx
@@ -67,14 +67,20 @@ function App() {
     });
   }
 
+  const ctxValue = {
+    items: shoppingCart.items,
+    addItemToCart: handleAddItemToCart,
+  };
+
   return (
     /**
      *  createContext를 Header와 Shop을 감싸는 이유는
      *  자식 컴포넌트까지 영향을 컨테스트에 접속할 수 있기 때문이다.
      *  컴포넌트 뒤에 `.` 온점을 찍는 것은 특정 오브젝트 안에 중첩된 속성값이 실질적인 컴포넌트(중첩된 속성의 값이 유효한 리액트 컴포넌트)가 되는 경우 jsx 파일에 유효하다.
      *  컨텍스트를 생성할 때 설정한 기본 값은 Provider 컴포넌트로 감싸지지 않은 컴포넌트가 컨텍스트 값을 액세스하려고 할 때만 사용됩니다.
+     *  context value를 useState와 연결시키는 방법은
      */
-    <CartContext.Provider value={{ items: [] }}>
+    <CartContext.Provider value={ctxValue}>
       <Header
         cart={shoppingCart}
         onUpdateCartItemQuantity={handleUpdateCartItemQuantity}
--- 10 Advanced State Management with Context useReducer/01-starting-project/src/store/shopping-cart-context.jsx
+++ 10 Advanced State Management with Context useReducer/01-starting-project/src/store/shopping-cart-context.jsx
@@ -5,5 +5,6 @@ import { createContext } from "react";
  *  context를 적용할 컴포넌트들도 묶어줘야한다.
  */
 export const CartContext = createContext({
-  items: [],
+  items: [], // 값 불러오기
+  addItemToCart: () => {}, // 값 업데이트하는 함수
 });
