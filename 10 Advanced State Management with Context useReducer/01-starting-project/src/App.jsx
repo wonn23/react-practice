eb33917353d90baeeb0421f5a42278237c1f7aa9
heochangwon <wonn22@gmail.com>
2024-06-23 17:50:56 +0900

    컨텍스트 소비하기: useContext

--- 10 Advanced State Management with Context useReducer/01-starting-project/src/App.jsx
+++ 10 Advanced State Management with Context useReducer/01-starting-project/src/App.jsx
@@ -72,8 +72,9 @@ function App() {
      *  createContext를 Header와 Shop을 감싸는 이유는
      *  자식 컴포넌트까지 영향을 컨테스트에 접속할 수 있기 때문이다.
      *  컴포넌트 뒤에 `.` 온점을 찍는 것은 특정 오브젝트 안에 중첩된 속성값이 실질적인 컴포넌트(중첩된 속성의 값이 유효한 리액트 컴포넌트)가 되는 경우 jsx 파일에 유효하다.
+     *  컨텍스트를 생성할 때 설정한 기본 값은 Provider 컴포넌트로 감싸지지 않은 컴포넌트가 컨텍스트 값을 액세스하려고 할 때만 사용됩니다.
      */
-    <CartContext.Provider>
+    <CartContext.Provider value={{ items: [] }}>
       <Header
         cart={shoppingCart}
         onUpdateCartItemQuantity={handleUpdateCartItemQuantity}
@@ -82,7 +83,7 @@ function App() {
         <ul id="products">
           {DUMMY_PRODUCTS.map((product) => (
             <li key={product.id}>
-              <Product {...product} onAddToCart={onAddItemToCart} />
+              <Product {...product} onAddToCart={handleAddItemToCart} />
             </li>
           ))}
         </ul>
--- 10 Advanced State Management with Context useReducer/01-starting-project/src/components/Product.jsx
+++ 10 Advanced State Management with Context useReducer/01-starting-project/src/components/Product.jsx
@@ -12,10 +12,10 @@ export default function Product({
       <div className="product-content">
         <div>
           <h3>{title}</h3>
-          <p className='product-price'>${price}</p>
+          <p className="product-price">${price}</p>
           <p>{description}</p>
         </div>
-        <p className='product-actions'>
+        <p className="product-actions">
           <button onClick={() => onAddToCart(id)}>Add to Cart</button>
         </p>
       </div>
