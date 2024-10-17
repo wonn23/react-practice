32e841ad3016bcb015adfceb1b7ec82c13ddfc1a
heochangwon <wonn22@gmail.com>
2024-10-17 13:08:01 +0900

    리액트 퀴즈 프로젝트 완료

---
+++ 13 Demo Project - React Quiz/01-starting-project/src/components/Question.jsx
@@ -0,0 +1,50 @@
+import { useState } from "react";
+
+import QuestionTimer from "./QuestionTimer";
+import Answers from "./Answers";
+import QUESTIONS from "../questions";
+
+export default function Question({ index, onSelectAnswer, onSkipAnswer }) {
+  const [answer, setAnswer] = useState({ selectedAnswer: "", isCorrect: null });
+
+  const timer = 10000;
+
+  function handleSelectAnswer(answer) {
+    setAnswer({
+      selectedAnswer: answer,
+      isCorrect: null,
+    });
+
+    setTimeout(() => {
+      setAnswer({
+        selectedAnswer: answer,
+        isCorrect: QUESTIONS[index].answers[0] === answer,
+      });
+
+      setTimeout(() => {
+        onSelectAnswer(answer);
+      }, 2000);
+    }, 1000);
+  }
+
+  let answerState = "";
+
+  if (answer.selectedAnswer && answer.isCorrect !== null) {
+    answerState = answer.isCorrect ? "correct" : "wrong";
+  } else if (answer.selectedAnswer) {
+    answerState = "answered";
+  }
+
+  return (
+    <div id="question">
+      <QuestionTimer timeout={10000} onTimeout={onSkipAnswer} />
+      <h2>{QUESTIONS[index].text}</h2>;
+      <Answers
+        answers={QUESTIONS[index].answers}
+        selectedAnswer={answer.selectedAnswer}
+        answerState={answerState}
+        onSelect={handleSelectAnswer}
+      />
+    </div>
+  );
+}
